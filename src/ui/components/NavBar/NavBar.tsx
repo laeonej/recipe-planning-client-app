84f3314cca140a8c69d2e27518f4a6eea19353a8
Jeff Li <jeffli@Jeffs-MacBook-Pro.local>
2024-09-22 15:42:58 -0400

    KAN-73: redirect meal plan nav to meal plan endpoint

--- src/ui/components/NavBar/NavBar.tsx
+++ src/ui/components/NavBar/NavBar.tsx
@@ -15,7 +15,7 @@ const NavBar = () => {
             <ul className=' bg-orange-400 w-full h-16'>
                 <Nav routeTo={routes.ROOT} direction='left'><FaBowlFood className='h-6 w-auto'/></Nav>
                 <Nav routeTo={routes.SAMPLE} label='Recipes' direction='left'/>
-                { isAuthenticated && <Nav routeTo={routes.ROOT} label='Meal Plan' direction='left'/> }
+                { isAuthenticated && <Nav routeTo={routes.MEAL_PLAN} label='Meal Plan' direction='left'/> }
                 { isAuthenticated && userId && <Nav routeTo={routes.ROOT} direction='right'><FaRegUserCircle className='h-6 w-auto'/></Nav> }
                 { !isAuthenticated && <Nav routeTo={routes.LOGIN} label='Login' direction='right'/> }
                 { !isAuthenticated && <Nav routeTo={routes.SIGNUP} label='Sign Up' direction='right'/> }
